138a912c0434e20c10fde1884d29a85791774df0
devbwoh <bwoh@outlook.kr>
2026-09-20 20:54:01 +0900

    🗃️ 파일 저장 및 로드

--- G26W03MFC/G26W03MFCDoc.h
+++ G26W03MFC/G26W03MFCDoc.h
@@ -12,7 +12,10 @@ protected:
 	CPoint Point = CPoint(-100, -100);
 public:
 	CPoint GetPoint() { return Point; }
-	void SetPoint(CPoint p) { Point = p; }
+	void SetPoint(CPoint p) { 
+		Point = p; 
+		SetModifiedFlag();
+	}
 
 protected: // serialization에서만 만들어집니다.
 	CG26W03MFCDoc() noexcept;
